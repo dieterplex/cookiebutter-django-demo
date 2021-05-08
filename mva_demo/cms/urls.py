cf92aa4710fb8426485b7e619014bf4d6daaafc4
d1t2 <dieterplex@gmail.com>
2021-05-08 13:57:01 +0800

    Un/like song  and notification

--- mva_demo/cms/urls.py
+++ mva_demo/cms/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from .views import song_all, song_detail, category_list
+from .views import (
+    song_all,
+    song_detail,
+    category_list,
+    like_song,
+    unlike_song
+)
 
 app_name = 'cms'
 
@@ -8,4 +14,7 @@ urlpatterns = [
     path('', song_all, name='song_all'),
     path('<slug:slug>', song_detail, name='song_detail'),
     path('category/<slug:category_slug>/', category_list, name='category_list'),
+
+    path('like/<slug:slug>/', like_song, name="like_song"),
+    path('unlike/<slug:slug>/', unlike_song, name="unlike_song")
 ]
--- mva_demo/cms/views.py
+++ mva_demo/cms/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from notifications.models import Notification
+from notifications.signals import notify
+
+from mva_demo.users.models import User
 
 from .models import Category, Song
-# from .models import Song
 
 
 def song_all(request):
     songs = Song.songs.all()
-    return render(request, 'cms/home.html', {'songs': songs})
+    return render(request, 'cms/songs/index.html', {'songs': songs})
 
 def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
@@ -15,4 +22,27 @@ def category_list(request, category_slug=None):
 
 def song_detail(request, slug):
     song = get_object_or_404(Song, slug=slug)
-    return render(request, 'cms/songs/single.html', {'song': song, 'like': False})
+    user = User.objects.get(username=request.user)
+    notif = user.notifications.filter(target_object_id=song.id).first()
+    like = notif is not None and notif.verb=="like"
+    return render(request, 'cms/songs/single.html', {'song': song, 'like': like})
+
+@login_required
+@require_POST
+def like_song(request, slug):
+    song = get_object_or_404(Song, slug=slug)
+    sender = User.objects.get(username=request.user)
+    notify.send(sender, recipient=song.created_by, target=song, verb='like')
+    
+    data = {"status": "ok", "target": song.slug, "action": "like"}
+    return JsonResponse(data, safe=False)
+
+@login_required
+@require_POST
+def unlike_song(request, slug):
+    song = get_object_or_404(Song, slug=slug)
+    sender = User.objects.get(username=request.user)
+    notify.send(sender, recipient=song.created_by, target=song, verb='unlike')
+
+    data = {"status": "ok", "target": song.slug, "action": "unlike"}
+    return JsonResponse(data, safe=False)
