56a76328e4fecdd4dcb7ace28a4539742c5b5f10
d1t2 <dieterplex@gmail.com>
2021-05-10 10:11:50 +0800

    Refine user notifications page

--- mva_demo/users/urls.py
+++ mva_demo/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from mva_demo.users.views import (
     user_detail_view,
+    user_notification_view,
     user_redirect_view,
     user_update_view,
 )
@@ -10,5 +11,6 @@ app_name = "users"
 urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
+    path("~notification/", view=user_notification_view, name="notification"),
     path("<str:username>/", view=user_detail_view, name="detail"),
 ]
--- mva_demo/users/views.py
+++ mva_demo/users/views.py
@@ -4,6 +4,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView, RedirectView, UpdateView
+from notifications.views import AllNotificationsList
 
 User = get_user_model()
 
@@ -43,3 +44,9 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
 
 user_redirect_view = UserRedirectView.as_view()
+
+
+class UserNotificationsList(AllNotificationsList):
+    template_name = 'users/user_notifications.html'
+
+user_notification_view = UserNotificationsList.as_view()
